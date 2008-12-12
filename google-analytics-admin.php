4edc5aada70269ccd7c4b5926e407795e8a0b364
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-12 14:46:42 +0000

    import plugins and sem-theme
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/google-analytics/trunk@1 6c9c31c4-cb3d-4424-af3d-876279169c7e

---
+++ google-analytics-admin.php
@@ -0,0 +1,268 @@
+<?php
+class google_analytics_admin
+{
+	#
+	# init()
+	#
+
+	function init()
+	{
+		add_action('admin_menu', array('google_analytics_admin', 'add_option_page'));
+
+		add_filter('sem_api_key_protected', array('google_analytics_admin', 'sem_api_key_protected'));
+	} # init()
+	
+		
+	#
+	# sem_api_key_protected()
+	#
+	
+	function sem_api_key_protected($array)
+	{
+		$array[] = 'http://www.semiologic.com/media/software/marketing/google-analytics/google-analytics.zip';
+		
+		return $array;
+	} # sem_api_key_protected()
+
+
+	#
+	# add_option_page()
+	#
+
+	function add_option_page()
+	{
+		add_options_page(
+				__('Google&nbsp;Analytics'),
+				__('Google&nbsp;Analytics'),
+				'manage_options',
+				__FILE__,
+				array('google_analytics_admin', 'display_options')
+				);
+	} # add_option_page()
+
+
+	#
+	# update_options()
+	#
+
+	function update_options()
+	{
+		check_admin_referer('google_analytics');
+		
+		$o = array();
+
+		$ga_script = stripslashes($_POST['ga_script']);
+		
+		if ( preg_match("/
+				_gat\._getTracker\(\"(UA-\d+-\d+)\"\);
+				/ix", $ga_script, $match)
+			)
+		{
+			$o['uacct'] = $match[1];
+		}
+		else
+		{
+			$o['uacct'] = false;
+		}
+
+		update_option('google_analytics', $o);
+	} # update_options()
+
+
+	#
+	# display_options()
+	#
+
+	function display_options()
+	{
+		# Process updates, if any
+
+		if ( $_POST['update_google_analytics'] )
+		{
+			google_analytics_admin::update_options();
+
+			echo '<div class="updated">' . "\n"
+				. '<p>'
+					. '<strong>'
+					. __('Settings saved.', 'google-analytics')
+					. '</strong>'
+				. '</p>' . "\n"
+				. '</div>' . "\n";
+		}
+
+		$options = google_analytics::get_options();
+
+		$ga_script = <<<EOF
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("{uacct}");
+pageTracker._initData();
+pageTracker._trackPageview();
+</script>
+EOF;
+
+		if ( $options['uacct'] )
+		{
+			$ga_script = str_replace('{uacct}', $options['uacct'], $ga_script);
+		}
+		else
+		{
+			$ga_script = str_replace('{uacct}', __('Your Account ID'), $ga_script);
+		}
+
+		echo '<div class="wrap">' . "\n"
+			. "<h2>" . __('Google Analytics Settings', 'google-analytics') . "</h2>\n"
+			. '<form method="post" action="">' . "\n"
+			. '<input type="hidden" name="update_google_analytics" value="1" />' . "\n";
+
+		if ( function_exists('wp_nonce_field') ) wp_nonce_field('google_analytics');
+
+		echo '<table class="form-table">' . "\n";
+		
+		echo '<tr valign="top">'
+			. '<th scope="row">'
+			. __('GA Script', 'google-analytics')
+			. '</th>'
+			. '<td>'
+			. __('Paste the <b>ga.js</b> script from <a href="http://analytics.google.com">Google analytics</a> into the following textarea (<a href="http://www.google.com/support/googleanalytics/bin/answer.py?answer=55603">where do I find it?</a>):', 'google-analytics')
+			. '<br />' ."\n"
+			. '<textarea name="ga_script"'
+					. ' class="code" cols="58" rows="12" style="width: 95%;"'
+					. ' onfocus="var this_val=eval(this); this_val.select();"'
+					. '>'
+				. format_to_edit($ga_script)
+			. '</textarea>'
+			. '<p>'
+			. __('<strong>Important</strong>: Use the new <strong>ga.js</strong> script, not the legacy urchin.js script.')
+			. '</p>'
+			. '</td>'
+			. '</tr>';
+		
+		echo "</table>\n";
+		
+		echo '<p class="submit">'
+			. '<input type="submit"'
+				. ' value="' . attribute_escape(__('Save Changes')) . '"'
+				. " />"
+			. "</p>\n";
+
+		echo "</form>\n";
+
+		echo "<h3>" . __('Quick Reference', 'google-analytics') . "</h3>\n"
+			. '<table class="form-table">';
+
+		echo '<tr>'
+			. '<th scope="row">'
+			. '<p>' . 'Special Users' . '</p>'
+			. '</th>'
+			. '<td>';
+
+		$str = <<<EOF
+<p>Authors, editors and site admins are not tracked when logged in.</p>
+EOF;
+
+		echo __($str);
+		
+		echo '</td></tr>';
+
+		echo '<tr>'
+			. '<th scope="row">'
+			. '<p>' . 'Special Urls' . '</p>'
+			. '</th>'
+			. '<td>';
+
+		$str = <<<EOF
+<p>The Google Analytics plugin tracks a few special use-cases differently, for quick reference:</p>
+<ul>
+<li><a href="http://www.semiologic.com/software/newsletter-manager/">Mailing list subscriptions</a> get tracked as /subscription/[url]</li>
+<li>Search queries get tracked as /search/[keywords]</li>
+<li>404 errors (page not found) get tracked as /404/[url]</li>
+<li>File downloads get tracked as /file/[url]</li>
+<li>Clicks on outbound links get tracked as /outbound/[url]</li>
+</ul>
+EOF;
+
+		echo __($str);
+		
+		echo '</td></tr>';
+
+		echo '<tr>'
+			. '<th scope="row">'
+			. '<p>' . 'Site Usage Patterns' . '</p>'
+			. '</th>'
+			. '<td>';
+
+		$str = <<<EOF
+<p>Make sure to spot Google Analytics's navigation summary and site overlay reports for individual urls. They're located to the right when you visit any url's statistics.</p>
+<ul>
+	<li>The first is a concise report. It lets you know, at a glance, where users came from upon entering the page, and where they left upon leaving it.</li>
+	<li>The second is more graphical. It outputs your page with an overlay that shows where visitors are clicking.</li>
+</ul>
+EOF;
+
+		echo __($str);
+		
+		echo '</td></tr>';
+
+		echo '<tr>'
+			. '<th scope="row">'
+			. '<p>' . 'Advanced Tracking' . '</p>'
+			. '</th>'
+			. '<td>';
+
+		$str = <<<EOF
+<p><a href="http://www.getsemiologic.com">Semiologic Pro</a> lets you easily insert scripts on your site.</p>
+
+<p>To insert extra site-wide tracking code, browse Design / Footer, and add the relevant lines. Examples:</p>
+<ul>
+	<li><a href="http://code.google.com/apis/analytics/docs/gaJSApiBasicConfiguration.html#_gat.GA_Tracker_._setSampleRate">Set the sampling rate</a> at 50% on a high traffic site (this speeds up GA reports quite a bit):<br />
+	<textarea cols="58" rows="5" style="width: 95%;" class="code" onfocus="var this_val=eval(this); this_val.select();">
+&lt;script type="text/javascript"&gt;
+pageTracker._setSampleRate(50);
+&lt;/script&gt;
+</textarea>
+	</li>
+	<li><a href="http://code.google.com/apis/analytics/docs/gaTracking.html#Multiple">Share your stats</a> with your sales team's Google Analytics account:<br />
+	<textarea cols="58" rows="5" style="width: 95%;" class="code" onfocus="var this_val=eval(this); this_val.select();">
+&lt;script type="text/javascript"&gt;
+var trackerSalesDept = _gat.getTracker("UA-12345-1");
+trackerSalesDept._initData();
+trackerSalesDept._trackPageview();
+&lt;/script&gt;
+</textarea>
+	</li>
+</ul>
+<p>To insert extra page-specific tracking code, open "Footer" in the editor, and add the relevant lines. Examples:</p>
+	<ul>
+	<li><a href="http://code.google.com/apis/analytics/docs/gaJSApiBasicConfiguration.html#_gat.GA_Tracker_._setVar">Segment visitors</a> who visit your "Thank you for subscribing" page:<br />
+	<textarea cols="58" rows="5" style="width: 95%;" class="code" onfocus="var this_val=eval(this); this_val.select();">
+&lt;script type="text/javascript"&gt;
+pageTracker._setVar("Prospect");
+&lt;/script&gt;
+</textarea>
+	</li>
+	<li><a href="http://code.google.com/apis/analytics/docs/gaJSApiBasicConfiguration.html#_gat.GA_Tracker_._setSessionTimeout">Increase the session duration</a> on a very long sales copy that visitors actually read:<br />
+	<textarea cols="58" rows="5" style="width: 95%;" class="code" onfocus="var this_val=eval(this); this_val.select();">
+&lt;script type="text/javascript"&gt;
+pageTracker._setSessionTimeout(3600 * 2); // 2 hours
+&lt;/script&gt;
+</textarea>
+	</li>
+</ul>
+EOF;
+
+		echo __($str);
+		
+		echo '</td></tr>';
+
+		echo '</table>';
+
+		echo "</div>\n";
+	} # display_options()
+} # google_analytics_admin
+
+google_analytics_admin::init();
+?>
---
+++ google-analytics.php
@@ -0,0 +1,210 @@
+<?php
+/*
+Plugin Name: Google Analytics
+Plugin URI: http://www.semiologic.com/software/marketing/google-analytics/
+Description: Adds <a href="http://analytics.google.com">Google analytics</a> to your blog, with various advanced tracking features enabled.
+Author: Denis de Bernardy
+Version: 3.1.1
+Author URI: http://www.semiologic.com
+Update Service: http://version.semiologic.com/wordpress
+Update Tag: google_analytics
+Update Package: http://www.semiologic.com/media/software/marketing/google-analytics/google-analytics.zip
+*/
+
+/*
+Terms of use
+------------
+
+This software is copyright Mesoconcepts Ltd, and is distributed under the terms of the Mesoconcepts license. In a nutshell, you may freely use it for any purpose, but may not redistribute it without written permission.
+
+http://www.mesoconcepts.com/license/
+**/
+
+
+load_plugin_textdomain('google-analytics');
+
+
+class google_analytics
+{
+	#
+	# init()
+	#
+
+	function init()
+	{
+		add_action('wp_footer', array('google_analytics', 'display_script'), 0);
+	} # init()
+
+
+	#
+	# get_options()
+	#
+
+	function get_options()
+	{
+		if ( ( $o = get_option('google_analytics') ) === false )
+		{
+			if ( ( $o = get_option('sem_google_analytics_params') ) !== false
+				&& is_string($o['script'])
+				&& preg_match("/
+					_uacct\s+=\s+\"([^\"]+)\";
+					/ix", $o['script'], $match)
+				)
+			{
+				$o = array(
+					'uacct' => $match[1]
+					);
+			}
+			else
+			{
+				$o = array(
+					'uacct' => false
+					);
+			}
+			
+			update_option('google_analytics', $o);
+		}
+		
+		return $o;
+	} # get_options()
+
+
+	#
+	# display_script()
+	#
+
+	function display_script()
+	{
+		$options = google_analytics::get_options();
+
+		if ( !$options['uacct'] )
+		{
+			echo __('<!-- Configure Google Analytics under Settings / Google Analytics -->') . "\n";
+			$script = str_replace('{uacct}', $options['uacct'], $script);
+
+			return;
+		}
+
+		$url = $_SERVER['REQUEST_URI'];
+		$url = preg_replace("/#.*$/", '', $url);
+
+		if ( isset($_GET['subscribed']) )
+		{
+			$url = '/subscription' . preg_replace("/(?:\?|&)subscribed/", "", $url);
+		}
+		elseif ( is_404() || ( is_singular() && !have_posts() ) )
+		{
+			$url = '/404' . $url;
+		}
+		elseif ( is_search() )
+		{
+			$url = "/search/" . urlencode($_REQUEST['s']);
+		}
+		
+		$ga_script = <<<EOF
+
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+var pageTracker = _gat._getTracker("{uacct}");
+
+
+EOF;
+		if ( current_user_can('publish_posts') )
+		{
+			echo "\n" . __('<!-- You are a site author, editor or admin, and aren\'t tracked as a result -->');
+			$ga_script .= '// ';
+		}
+
+		$ga_script .= <<<EOF
+pageTracker._trackPageview("{url}");
+</script>
+
+EOF;
+
+		$ga_script = str_replace(array('{url}', '{uacct}'), array($url, $options['uacct']), $ga_script);
+
+		echo $ga_script;
+		
+		if ( !current_user_can('publish_posts') || true )
+		{
+			$evt_script = <<<EOF
+
+<script type="text/javascript">
+//
+// GA Automated Event Tracking
+// ===========================
+// (c) 2008, Mesoconcepts Ltd
+// http://www.mesoconcepts.com/license/
+//
+
+var i = 0;
+
+// this should catch stuff on the same domain
+var ga_base_url_regexp = new RegExp("^.+?://" + document.domain, "i");
+
+// this should catch anything that looks more or less like a file
+var ga_file_regexp = new RegExp("\\\\.(?:phps|inc|js|css|exe|com|dll|reg|jpg|jpeg|gif|png|zip|tar\\.gz|tgz|mp3|wav|mpeg|avi|mov|swf|pdf|doc|rtf|xls|txt|csv)(?:\\\\?.*)?$", "i");
+
+// automatically track relevant anchors
+function ga_track_anchor()
+{
+	var url = new String(this.href);
+	url = url.replace(new RegExp("#.*$"), '');
+	
+	if ( !url.match(ga_base_url_regexp) )
+	{
+		url = url.replace(new RegExp("^.+?://(www\\\\.)?", "i"), '/');
+		url = "/outbound" + url;
+		//alert(url);
+		pageTracker._trackPageview(url);
+	}
+	else if ( url.match(ga_file_regexp) )
+	{
+		url = url.replace(ga_base_url_regexp, '');
+		url = '/file' + url;
+		//alert(url);
+		pageTracker._trackPageview(url);
+	}
+}
+
+// add the above method to every anchor
+for ( i = 0; i < document.getElementsByTagName("a").length; i++ )
+{
+	document.getElementsByTagName("a")[i].ga_track = ga_track_anchor;
+	
+	var oldonclick = document.getElementsByTagName("a")[i].onclick;	
+	if ( typeof document.getElementsByTagName("a")[i].onclick == 'function' )
+	{
+		document.getElementsByTagName("a")[i].onclick = function() { 
+			oldonclick(); 
+			this.ga_track(); 
+		}
+	}
+	else
+	{
+		document.getElementsByTagName("a")[i].onclick = function() { 
+			this.ga_track(); 
+		}
+	}	
+}
+
+</script>
+
+EOF;
+			echo $evt_script;
+		}
+	} # display_script()
+} # google_analytics()
+
+google_analytics::init();
+
+
+# include admin stuff when relevant
+if ( is_admin() )
+{
+	include dirname(__FILE__) . '/google-analytics-admin.php';
+}
+?>

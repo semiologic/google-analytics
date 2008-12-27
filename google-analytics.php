00667a6d29a3d9fcbf6b8a0a97f5f1a76a942720
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-27 11:50:46 +0000

    bump versions
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/google-analytics/trunk@331 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- google-analytics.php
+++ google-analytics.php
@@ -4,7 +4,7 @@ Plugin Name: Google Analytics
 Plugin URI: http://www.semiologic.com/software/marketing/google-analytics/
 Description: Adds <a href="http://analytics.google.com">Google analytics</a> to your blog, with various advanced tracking features enabled.
 Author: Denis de Bernardy
-Version: 3.1.2 beta
+Version: 3.1.2 RC
 Author URI: http://www.getsemiologic.com
 Update Service: http://version.semiologic.com/plugins
 Update Tag: google_analytics

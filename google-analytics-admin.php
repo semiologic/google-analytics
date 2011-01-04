b8221c2dfe679b04c24b38873fcc5b5c4e580eb1
Denis de Bernardy <ddebarnardy@yahoo.com>
2011-01-04 15:51:09 +0000

    dump dead code
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/google-analytics/trunk@4576 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- google-analytics-admin.php
+++ google-analytics-admin.php
@@ -50,19 +50,6 @@ class google_analytics_admin {
 		if ( !$uacct )
 			$uacct = __('Your Account ID', 'google-analytics');
 		
-		$ga_script = <<<EOF
-<script type="text/javascript">
-var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
-document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
-</script>
-<script type="text/javascript">
-try {
-var pageTracker = _gat._getTracker("$uacct");
-pageTracker._trackPageview();
-} catch(err) {}
-</script>
-EOF;
-		
 		echo '<div class="wrap">' . "\n"
 			. '<form method="post" action="">' . "\n";
 		
